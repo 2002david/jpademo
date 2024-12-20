745100df053780c4451dd14129fea9d0e4bfe1a0
David Steglich <davidsteglich@outlook.de>
2024-12-20 16:41:14 +0100

    bug fix

--- src/main/java/org/hbrs/jpademo/Main.java
+++ src/main/java/org/hbrs/jpademo/Main.java
@@ -2,6 +2,7 @@ package org.hbrs.jpademo;
 
 import org.hbrs.jpademo.model.Lehrer;
 import org.hbrs.jpademo.model.Person;
+import org.hbrs.jpademo.model.Schueler;
 
 import java.time.LocalDateTime;
 
@@ -10,16 +11,27 @@ public class Main {
         System.out.println("Starting jpademo - " + LocalDateTime.now());
 
         DAO<Lehrer> lehrerDAO = new DAO<>(Lehrer.class);
+        DAO<Schueler> schuelerDAO = new DAO<>(Schueler.class);
 
+        // Lehrer
         Lehrer lehrer = new Lehrer();
-        lehrer.setPersonId(0);
         lehrer.setVorname("Max");
         lehrer.setNachname("Mustermann");
-        lehrer.setGeburtsdatum(java.sql.Date.valueOf("1970-01-01"));
-        lehrer.setGehalt(5000.0);
+        lehrer.setGeburtsdatum(java.sql.Date.valueOf("1970-01-02"));
+        lehrer.setGehalt(6000.0);
 
         lehrerDAO.save(lehrer);
 
+        // Schueler
+        Schueler schueler = new Schueler();
+        schueler.setVorname("Erika");
+        schueler.setNachname("Musterman");
+        schueler.setGeburtsdatum(java.sql.Date.valueOf("1970-01-03"));
+        schueler.setEinschulungsjahr(java.sql.Date.valueOf("1976-01-02"));
+
+        schuelerDAO.save(schueler);
+
+        // find "Lehrer"
         Lehrer savedLehrer = lehrerDAO.find(lehrer.getPersonId());
         System.out.println(savedLehrer.getVorname());
     }
